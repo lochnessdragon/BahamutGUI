9ddc29a1896945e707ffbc9070e726dd3bfa0323
lochnessdragon <39635811+lochnessdragon@users.noreply.github.com>
2021-12-01 09:25:43 -0600

    Removed whitespace

--- code_coverage.sh
+++ code_coverage.sh
@@ -9,15 +9,10 @@ done
 #-------------------------------------------
 # OLD CODE COVERAGE STUFF BELOW
 #-------------------------------------------
-
 # make sure that the tests have been run
 #./bin/Debug-linux-x86_64/tests/tests
-
 #mkdir -p code_coverage
 #cd code_coverage/
-
 #gcov -o ../bin-int/Debug-linux-x86_64/tests ../tests/*.cpp
-
 #lcov --directory ../bin-int/Debug-linux-x86_64/tests --directory ../bin-int/Debug-linux-x86_64/bahamutGUI --output-file main_coverage.info -c
-
 #genhtml main_coverage.info --output-directory out
